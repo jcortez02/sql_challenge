d0c25b997ea7ab9d3030f92c0529faccdd7f07a1
KDot <jacob.joshua.cortez@icloud.com>
2021-11-13 15:25:25 -0600

    Forgot to add Not Null

--- sql_homework.sql
+++ sql_homework.sql
@@ -1,38 +1,38 @@
 ------ Schema (base)
 
 CREATE TABLE departments
-(dept_no VARCHAR,
-dept_name VARCHAR
+(dept_no VARCHAR NOT NULL,
+dept_name VARCHAR NOT NULL
 );
 
 CREATE TABLE dept_emp
-(emp_no INT,
-dept_no VARCHAR
+(emp_no INT NOT NULL,
+dept_no VARCHAR NOT NULL
 );
 
 CREATE TABLE dept_manager
-(dept_no VARCHAR,
-emp_no INT
+(dept_no VARCHAR NOT NULL,
+emp_no INT NOT NULL
 );
 
 CREATE TABLE employees
-(emp_no INT,
-emp_title VARCHAR,
-birth_date DATE,
-first_name VARCHAR,
-last_name VARCHAR,
-sex VARCHAR,
-hire_date DATE
+(emp_no INT NOT NULL,
+emp_title VARCHAR NOT NULL,
+birth_date DATE NOT NULL,
+first_name VARCHAR NOT NULL,
+last_name VARCHAR NOT NULL,
+sex VARCHAR NOT NULL,
+hire_date DATE NOT NULL
 );
 
 CREATE TABLE salaries
-(emp_no INT,
-salary INT
+(emp_no INT NOT NULL,
+salary INT NOT NULL
 );
 
 CREATE TABLE titles
-(title_id VARCHAR,
-title VARCHAR
+(title_id VARCHAR NOT NULL,
+title VARCHAR NOT NULL
 );
 
 
